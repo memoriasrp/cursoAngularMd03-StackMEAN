5f14a717e177e46d86341eda7222090bee282e2b
Silvia Rodriguez <silviamarp@gmail.com>
2026-03-22 17:54:26 -0500

    agregando funciones asincronas como un reloj

--- lugare-bonitos/src/app/app.ts
+++ lugare-bonitos/src/app/app.ts
@@ -1,14 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ListaDestinos } from './lista-destinos/lista-destinos';
+import { AsyncPipe } from '@angular/common';
+
+import { interval, map } from 'rxjs';
+
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, ListaDestinos],
+  imports: [RouterOutlet, RouterLink, ListaDestinos, AsyncPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('lugare-bonitos');
+  time = interval(1000).pipe(
+    map(() => new Date().toLocaleString())
+  );
+
+
 }
